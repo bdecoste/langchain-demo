2207a97e6858104b180b177f31f7beb84f5e7fef
Bill DeCoste <bdecoste@gmail.com>
2026-05-16 12:51:07 -0700

    update

---
+++ experiment.py
@@ -0,0 +1,57 @@
+from langsmith import Client, wrappers
+from openevals.llm import create_llm_as_judge
+from openevals.prompts import CORRECTNESS_PROMPT
+from openai import OpenAI
+
+client = Client()
+
+dataset = client.create_dataset(
+    dataset_name="ds-artistic-fruit-3", description="A sample dataset in LangSmith."
+)
+examples = [
+    {
+        "inputs": {"question": "Which country is Mount Kilimanjaro located in?"},
+        "outputs": {"answer": "Mount Kilimanjaro is located in Tanzania."},
+    },
+    {
+        "inputs": {"question": "What is Earth's lowest point?"},
+        "outputs": {"answer": "Earth's lowest point is The Dead Sea."},
+    },
+]
+client.create_examples(dataset_id=dataset.id, examples=examples)
+
+# Wrap the OpenAI client for LangSmith tracing
+openai_client = wrappers.wrap_openai(OpenAI())
+
+
+# Define the application logic to evaluate.
+# Dataset inputs are automatically sent to this target function.
+def target(inputs: dict) -> dict:
+    response = openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Answer the following question accurately"},
+            {"role": "user", "content": inputs["question"]},
+        ],
+    )
+    return {"answer": response.choices[0].message.content}
+
+# Define an LLM-as-a-judge evaluator to evaluate correctness of the output
+def correctness_evaluator(inputs: dict, outputs: dict, reference_outputs: dict):
+    evaluator = create_llm_as_judge(
+        prompt=CORRECTNESS_PROMPT,
+        model="openai:o3-mini",
+        feedback_key="correctness",
+    )
+    eval_result = evaluator(
+        inputs=inputs, outputs=outputs, reference_outputs=reference_outputs
+    )
+    return eval_result
+
+experiment_results = client.evaluate(
+    target,
+    data="ds-artistic-fruit-3",
+    evaluators=[correctness_evaluator],
+    experiment_prefix="experiment-quickstart-minty-realization-10",
+    max_concurrency=2,
+)
